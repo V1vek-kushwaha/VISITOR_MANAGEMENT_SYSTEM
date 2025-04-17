ba6530ec9c098b9b0fd25bd0e30cce411596ced6
Akash Tiwari <akashti821032@gmail.com>
2025-04-17 16:00:06 +0530

    file added

--- backend vms/config/Database/db.config.js
+++ backend vms/config/Database/db.config.js
@@ -15,11 +15,11 @@ require("dotenv").config();
 // };
 
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "admin@123",
-  DB: "vivek",
-  dialect: "mysql",
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
   pool: {
     max: 5,
     min: 0,
